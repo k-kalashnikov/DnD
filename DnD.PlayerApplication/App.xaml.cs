be68c78803078ac0d90571a30b0b3740fab5f799
Kalashnikov Constantine <k-kalashnikov73@yandex.ru>
2022-05-31 21:23:20 +0400

    Add empty application project

---
+++ DnD.PlayerApplication/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DnD.PlayerApplication
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ DnD.PlayerApplication/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DnD.PlayerApplication
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
