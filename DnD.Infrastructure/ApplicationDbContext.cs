8f268f4e5d5eac1e9038b47e8a1eb47078b391ea
Kalashnikov Constantine <k-kalashnikov73@yandex.ru>
2022-06-17 02:01:22 +0400

    Add layers projects Add first DbContext and factory

---
+++ DnD.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DnD.Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ DnD.Infrastructure/ApplicationDbContextFactory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace DnD.Infrastructure
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                throw new ArgumentException("Please enter configuration folder path. ", "args");
+            }
+
+            var currentPath = Path.GetFullPath(args[0]);
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(currentPath)
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("SqliteConnectionString");
+
+            Console.WriteLine($"Connection string is = {connectionString}");
+
+            var optionBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionBuilder.UseSqlite(connectionString);
+
+            return new ApplicationDbContext(optionBuilder.Options);
+        }
+    }
+}
